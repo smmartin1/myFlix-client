8b8b2fac23de8723c72daafbdfd70b80c227959f
smmartin1 <smarie333m@gmail.com>
2022-05-02 11:03:45 -0400

    Remapped to MainView
    Able to display Main View

--- src/index.jsx
+++ src/index.jsx
@@ -1,21 +1,23 @@
+//Import React
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Import index.scss
+//Import Main View
+import { MainView } from './components/main-view/main-view';
+
+//Import index.scss
 import './index.scss';
 
 // Main component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
+      <MainView />
     );
   }
 }
 
-// Finds root of the app
+//Finds root of the app
 const container = document.getElementsByClassName('app-container')[0];
 
 //Render app in the root DOM element
