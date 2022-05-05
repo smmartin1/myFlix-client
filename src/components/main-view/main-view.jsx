f307eda767634987ed6deabd452ada2d82389548
smmartin1 <smarie333m@gmail.com>
2022-05-05 11:17:56 -0400

    Multiple Changes
    Added Axios and PropTypes
    Imported Login-View and Registration-View
    Added Movies collection

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,59 +10,62 @@ export class MainView extends React.Component{
   constructor(){
       super();
       this.state = {
-        movies: [
-          {
-            _id: 1,
-            Title: 'Lord of The Rings: The Fellowship of The Rings',
-            Description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-            Genre: 'Fantasy',
-            Director: 'Peter Jackson',
-            ImagePath: 'https://i.ytimg.com/vi_webp/l2NZzop5Xbw/movieposter_en.webp'
-          },
-          {
-            _id: 2,
-            Title: 'How To Train Your Dragon',
-            Description: 'A hapless young Viking who aspires to hunt dragons becomes the unlikely friend of a young dragon himself, and learns there may be more to the creatures than he assumed.',
-            Genre: 'Animation',
-            Director: 'Chris Sanders',
-            ImagePath: 'https://i.ytimg.com/vi/MyZ4ZBUqtSs/movieposter.jpg'
-          },
-          {
-            _id: 3,
-            Title: 'Jurassic Park',
-            Description: 'A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park\'s cloned dinosaurs to run loose.',
-            Genre: 'Sci Fi',
-            Director: 'Steven Spielburg',
-            ImagePath: 'https://i.ytimg.com/vi_webp/f5C7dqrAItM/movieposter.webp'
-          }
-        ],
-        selectedMovie: null
+        movies: [],
+        selectedMovie: null,
+        user: null,
+        register: null
       };
     }
 
-    setSelectedMovie(newSelectedMovie) {
+    componentDidMount(){
+      axios.get('https://fathomless-peak-84165.herokuapp.com/movies')
+        .then(response => {
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+
+    setSelectedMovie(movie) {
       this.setState({
-        selectedMovie: newSelectedMovie
+        selectedMovie: movie
+      });
+    }
+
+    onLoggedIn(user) {
+      this.setState({
+        user
+      });
+    }
+
+    onRegister(register) {
+      this.setState({
+        register
       });
     }
 
     render() {
-      const { movies, selectedMovie } = this.state;
+      const { movies, selectedMovie, user, register } = this.state;
 
-      if (movies.length === 0){
-        return <div className="main-view">The list is empty!</div>;
-      } else {
-        return (
-          <div className="main-view">
-            {selectedMovie
-              ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
-              : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-              ))
-            }
-          </div>
-        );
-      }
+      if (register) return <RegistrationView onRegister={(bool) => this.onRegister(bool)} />;
+
+      if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
+      if (movies.length === 0) return <div className="main-view" />;
+
+      return (
+        <div className="main-view">
+          {selectedMovie
+            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            ))
+          }
+        </div>
+      );
     }
 }
 
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render(){
@@ -6,3 +7,21 @@ export class MovieCard extends React.Component {
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string,
+    Description: PropTypes.string,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birth: PropTypes.string,
+      Death: PropTypes.string
+    })
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
